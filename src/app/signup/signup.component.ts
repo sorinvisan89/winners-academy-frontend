4ed959098a49173f34ae9c9fc4684072bd0c1c97
VIS73600 <Sorin.Visan@viavisolutions.com>
2019-06-26 10:49:12 +0300

    more improvements related to form validation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {SnackbarService} from 'ngx-snackbar';
+import {User} from '../models/user';
+import {UserService} from '../services/user.service';
 
 @Component({
-    selector: 'app-signup',
-    templateUrl: './signup.component.html',
-    styleUrls: ['./signup.component.scss']
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-    test : Date = new Date();
-    focus;
-    focus1;
-    focus2;
-    constructor() { }
+  focus;
+  focus1;
+  focus2;
 
-    ngOnInit() {}
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private snackbarService: SnackbarService,
+              private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      privacyPolicy: new FormControl(false, [Validators.pattern('true')])
+    });
+  }
+
+  registerUser() {
+    const newUser: User = this.signupForm.value;
+    this.userService.addUser(newUser).subscribe(
+      user => {
+
+      },
+      error => {
+        this.snackbarService.add({
+          msg: 'Unable to register user: ' + newUser.name,
+          action: {
+            text: 'Dismiss!'
+          }
+        });
+      },
+      () => {
+        this.snackbarService.add({
+          msg: 'Registered user: ' + newUser.name,
+          timeout: 5000,
+          action: {
+            text: 'Dismiss!'
+          }
+        });
+      });
+  }
 }
